184b753c084ec03e6a00aa3e578fbbcfa9844046
Andreas Ramalho Spyridakis <script.mux@pm.me>
2024-03-27 13:53:54 -0300

    Formatação de comentários.

--- src/main.py
+++ src/main.py
@@ -4,10 +4,17 @@ Descricao: arquivo principal do sistema `pi2a-olimpiadas-tenis-de-mesa`.
 '''
 import os
 
+
 def limpar_tela():
     """Limpar a tela do terminal."""
-    # Limpar a tela do terminal segundo o tipo de sistema operacional onde é executado:
-    os.system('cls' if os.name == 'nt' else 'clear') # 'nt' para Windows e 'clear' para qualquer outro (Linux, macOS, etc)
+    # Limpar a tela do terminal segundo o tipo de sistema operacional onde é
+    # executado:
+    os.system('cls' if os.name == 'nt' else 'clear')  # 'nt' para Windows e 'clear' para qualquer outro (Linux, macOS, etc)
+
+
+def escolher_opcao():
+    """Escolher uma opção de qualquer menu apresentado para o usuário."""
+    return input("\nEscolha uma opção: ")  # Retornar o valor da entrada do usuário (nesse caso, como string).
 
 # TODO: implementar funcionalidade "Criar partida" (dados locais)
 
@@ -19,12 +26,13 @@ def limpar_tela():
 
 # TODO: implementar funcionalidade "Enviar dados para servidor"
 
-def main():
 
+def main():
+    """Método main para iniciar o programa."""
     continuar = True
 
-    while (continuar):
-        limpar_tela() # Limpar a tela antes de exibir o menu
+    while continuar:
+        limpar_tela()  # Limpar a tela antes de exibir o menu
         print("\nInício do programa 'olimpiadas-tenis-de-mesa'."
               + "\n 0. Sair"
               + "\n 1. Criar nova partida e inserir dados"
@@ -32,20 +40,21 @@ def main():
               + "\n 3. Remover partida"
               + "\n 4. Enviar dados para servidor")
 
-        opcao = input("\nEscolha uma opção: ")
+        opcao = escolher_opcao()
 
-        if (opcao == "0"):
-            print("Saindo...")
-            break
-        elif (opcao == "1"):
-            print("Prosseguindo para criar nova partida e inserir dados")
-            input("Pressione Enter para continuar")
-        elif (opcao == "2"):
+        if opcao == "2":
             print("Prosseguindo para editar dados de partida")
             input("Pressione Enter para continuar...")
+        elif opcao == "1":
+            print("Prosseguindo para criar nova partida e inserir dados")
+            input("Pressione Enter para continuar")
+        elif opcao == "0":
+            print("Saindo...")
+            break
         else:
             print("Opção inválida. Tente novamente.")
             input("Pressione Enter para continuar...")
+
 
 if __name__ == "__main__":
     main()
